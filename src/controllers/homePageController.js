84a637f78cfaf70d1eca655cfa645b37398b86a9
KM04021987 <krishnendu_mckv@yahoo.co.in>
2022-06-28 14:08:07 -0500

    first commit

--- src/controllers/homePageController.js
+++ src/controllers/homePageController.js
@@ -5,6 +5,46 @@ let getHomePage = (req, res) => {
     });
 };
 
+let getAboutPage = (req, res) => {
+    console.log('homePageController: getAboutPage')
+    return res.render("about.ejs", {
+        errors: req.flash("errors")
+    });
+};
+
+let getRecyclableitemsPage = (req, res) => {
+    console.log('homePageController: getRecyclableitemsPage')
+    return res.render("recyclableitems.ejs", {
+        errors: req.flash("errors")
+    });
+};
+
+let getStepstofollowPage = (req, res) => {
+    console.log('homePageController: getStepstofollowPage')
+    return res.render("stepstofollow.ejs", {
+        errors: req.flash("errors")
+    });
+};
+
+let getPrivacyPage = (req, res) => {
+    console.log('homePageController: getPrivacyPage')
+    return res.render("Privacy.ejs", {
+        errors: req.flash("errors")
+    });
+};
+
+let getFaqPage = (req, res) => {
+    console.log('homePageController: getFaqPage')
+    return res.render("faq.ejs", {
+        errors: req.flash("errors")
+    });
+};
+
 module.exports = {
-    getHomePage: getHomePage
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getRecyclableitemsPage: getRecyclableitemsPage,
+    getStepstofollowPage: getStepstofollowPage,
+    getPrivacyPage: getPrivacyPage,
+    getFaqPage: getFaqPage
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,5 @@
 import express from "express";
 import homePageController from "../controllers/homePageController"
-import aboutPageController from "../controllers/aboutPageController"
-import recyclableitemsPageController from "../controllers/recyclableitemsPageController"
-import stepstofollowPageController from "../controllers/stepstofollowPageController"
-import privacyPageController from "../controllers/privacyPageController"
-import faqPageController from "../controllers/faqPageController"
 import donorProfileController from "../controllers/donorProfileController";
 import receiverProfileController from "../controllers/receiverProfileController";
 import donorregisterController from "../controllers/donorregisterController";
@@ -22,11 +17,22 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homePageController.getHomePage);
-    router.get("/about", aboutPageController.getAboutPage);
-    router.get("/recyclableitems", recyclableitemsPageController.getRecyclableitemsPage);
-    router.get("/stepstofollow", stepstofollowPageController.getStepstofollowPage);
-    router.get("/privacy", privacyPageController.getPrivacyPage);
-    router.get("/faq", faqPageController.getFaqPage);
+/*Home Page - Starts*/
+    router.get("/about", homePageController.getAboutPage);
+    router.get("/recyclableitems", homePageController.getRecyclableitemsPage);
+    router.get("/stepstofollow", homePageController.getStepstofollowPage);
+    router.get("/privacy", homePageController.getPrivacyPage);
+    router.get("/faq", homePageController.getFaqPage);
+/*Home Page - Ends*/
+
+/*Register Starts*/
+    router.get("/dregister", donorregisterController.getPageRegister);
+    router.get("/rregister", receiverregisterController.getPageRegister);
+    router.post("/dregister", auth.validateRegister, donorregisterController.createNewUser);
+    router.post("/rregister", auth.validateRegister, receiverregisterController.createNewUser);
+/*Register Ends*/
+
+/*Login Authentication - Starts*/
     router.get("/donorlogin.ejs", donorloginController.checkLoggedIn, donorProfileController.handleDonorPage);
     router.get("/receiverlogin.ejs", receiverloginController.checkLoggedIn, receiverProfileController.handleReceiverPage);
     router.get("/dlogin",donorloginController.checkLoggedOut, donorloginController.getPageLogin);
@@ -46,25 +52,30 @@ let initWebRoutes = (app) => {
         failureFlash: true
     }));
 
-    router.get("/dregister", donorregisterController.getPageRegister);
-    router.get("/rregister", receiverregisterController.getPageRegister);
-    router.post("/dregister", auth.validateRegister, donorregisterController.createNewUser);
-    router.post("/rregister", auth.validateRegister, receiverregisterController.createNewUser);
-    router.post("/logout", donorloginController.postLogOut);
-    router.post("/logout", receiverloginController.postLogOut);
-    router.post("/dprofile", donorProfileController.createPickupRequest);
     router.get("/dprofile", donorProfileController.handleDonorPage);
     router.get("/rprofile", receiverProfileController.handleReceiverPage);
+/*Login Authentication - Ends*/
 
+/*Donor's after login - Starts*/
+    router.post("/dprofile", donorProfileController.createPickupRequest);
+    
     router.get('/get-edit-pickup/:id', donorProfileController.getEditPickup);
     router.put('/put-edit-pickup', donorProfileController.putEditPickup);
     router.delete('/delete-pickup', donorProfileController.deletePickupById);
     router.post('/list-of-receivers', donorProfileController.getReceiverList);
+/*Donor's after login - Ends*/
 
+/*Receiver's after login - Starts*/
     router.get('/get-search-pickup-form/:id', receiverProfileController.getSearchPickupForm);
     router.post('/show-list-of-pickup/:id', receiverProfileController.showListOfPickup);
     router.post('/send-message/:id', receiverProfileController.sendMessage);
-    
+/*Receiver's after login - Ends*/
+
+/*Logout Starts*/
+    router.post("/logout", donorloginController.postLogOut);
+    router.post("/logout", receiverloginController.postLogOut);
+/*Logout Ends*/
+
     return app.use("/", router);
 };
 
